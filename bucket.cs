089e59a53acf900b51d506672bca3a85768e8076
AQdarkness <91960263+AQdarkness@users.noreply.github.com>
2024-08-03 13:53:48 +0800

    Add files via upload

---
+++ bucket.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class bucket : MonoBehaviour
+{
+    public Text myText;
+    
+    private int score = 0;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "rain")
+        {
+            
+            Debug.Log("your score");
+            score++;
+            myText.text = "your score is" + score;
+            //Destroy(gameObject);
+            Destroy(collision.gameObject);
+        }
+    
+
+    }
+    // Start is called before the first frame update
+    void Start()
+
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
